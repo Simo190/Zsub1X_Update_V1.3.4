c83f07bc1d06ef1f32b5ee7ff27a6bece8ce612a
Simone <37718696+Simo190@users.noreply.github.com>
2019-05-10 09:22:52 +0200

    Create ZsubUp.sh

---
+++ ZsubUp.sh
@@ -0,0 +1,15 @@
+wget https://github.com/Simo190/Zsub1X_Update_V1.3.4/releases/download/V1.4/zSub1x_1.4.0_Linux_daemon.zip
+unzip zSub1x_1.4.0_Linux_daemon.zip
+chmod +x zsub1x-cli
+chmod +x zsub1xd
+zsub1x-cli stop
+sleep 30
+killall -15 zsub1xd
+rm /usr/local/bin/zsub1x-cli
+rm /usr/local/bin/zsub1xd
+mv zsub1x-cli /usr/local/bin/
+mv zsub1xd /usr/local/bin/
+zsub1xd -daemon
+rm zSub1x_1.4.0_Linux_daemon.zip
+sleep 20
+watch zsub1x-cli mnsync status
